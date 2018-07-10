4af9c2822595c0bbeb9bd7f2b2e5c66d365c77b5
rekhapani <rekhasusan184@gmail.com>
2018-07-10 15:26:43 +0530

    # WARNING: head commit changed in the meantime
    Coomi

---
+++ ReferenceEntity/src/main/java/com/reference/commitment/entity/ReferenceEntityApplication.java
@@ -0,0 +1,12 @@
+package com.reference.commitment.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferenceEntityApplication.class, args);
+	}
+}
